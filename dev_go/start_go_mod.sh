4b4a6ad3ebfc192b88a1cdb38aaefd3d083173ee
qiuye2015 <1119345739@qq.com>
2021-02-10 13:15:38 +0800

    del ci

--- dev_go/start_go_mod.sh
+++ dev_go/start_go_mod.sh
@@ -1,9 +1,3 @@
-#########################################################################
-# File Name: start_go_mod.sh
-# Author: fjp
-# mail: fjp@xxx.com
-# Created Time: Tue 26 Jan 2021 11:20:24 AM CST
-#########################################################################
 #!/bin/bash
 mkdir bin
 mkdir pkg
