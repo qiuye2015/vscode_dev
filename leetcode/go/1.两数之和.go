9bc851684a605fe4ab238eb4bfba0e089fb25a81
qiuye <1119345739@qq.com>
2021-11-20 14:26:04 +0800

    towSum

---
+++ leetcode/go/1.两数之和.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	record:=make(map[int]int,0)
+	for idx,num:=range nums {
+		if val,ok:=record[target-num];ok{
+			return []int{val,idx}
+		}
+		record[num]=idx
+	}
+	return nil
+}
+// @lc code=end
+
