88fa03670b165625af23a1771f1743fc18a58dc9
qiuye <1119345739@qq.com>
2022-01-17 22:46:00 +0800

    [linked-list] 两数相加

---
+++ leetcode/go/2.两数相加.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=golang
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+import "leetcode_fjp/data_structures"
+
+type ListNode = data_structures.ListNode
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry, dummy := 0, new(ListNode)
+	for cur := dummy; l1 != nil || l2 != nil || carry > 0; cur = cur.Next {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+		cur.Next = &ListNode{Val: carry % 10, Next: nil}
+		carry = carry / 10
+	}
+	return dummy.Next
+}
+
+// @lc code=end
--- leetcode/go/main.go
+++ leetcode/go/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"leetcode_fjp/data_structures"
 )
 
 func main() {
 	// mainTwoSum()
+	// mainAddTwoNumbers()
 }
+
+//---------
 func mainTwoSum() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
 	res := twoSum(nums, target)
 	fmt.Println(res)
+}
+
+func mainAddTwoNumbers() {
+	nums1 := []int{9, 9, 9, 9, 9, 9, 9}
+	nums2 := []int{9, 9, 9, 9}
+	l1 := data_structures.Ints2List(nums1)
+	l2 := data_structures.Ints2List(nums2)
+	data_structures.PrintList(l1)
+	data_structures.PrintList(l2)
+	l3 := addTwoNumbers(l1, l2)
+	data_structures.PrintList(l3)
 }
