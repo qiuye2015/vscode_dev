dc2d6b3907dda73c2b152deb9d3482b91074f02d
qiuye <1119345739@qq.com>
2022-01-24 23:17:57 +0800

    0019.删除链表的倒数第 N 个结点

---
+++ leetcode/go/19.删除链表的倒数第-n-个结点.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+package main
+
+// type ListNode = data_structures.ListNode
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummyHead := &ListNode{-1, head}
+	fast, slow := dummyHead, dummyHead
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	toDel := slow.Next
+	slow.Next = toDel.Next
+	return dummyHead.Next
+}
+
+// @lc code=end
