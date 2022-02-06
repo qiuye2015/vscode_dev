cda5c1bf2a974c23b7b6b3f5e52ce7664b94b760
qiuye <1119345739@qq.com>
2022-02-07 00:24:35 +0800

    0092.反转链表-ii

---
+++ leetcode/go/92.反转链表-ii.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=golang
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		removed := cur.Next
+		cur.Next = removed.Next
+		removed.Next = pre.Next
+		pre.Next = removed
+	}
+	return dummy.Next
+}
+
+// @lc code=end
+
