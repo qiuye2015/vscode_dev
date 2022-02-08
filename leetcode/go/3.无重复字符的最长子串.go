07b8fa72ac8e5e5e5de24bdd6e01c20ca48d35ac
qiuye <1119345739@qq.com>
2022-02-08 22:41:07 +0800

    0003.无重复字符的最长子串

---
+++ leetcode/go/3.无重复字符的最长子串.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ */
+// package main
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	ret := 0
+	l := 0 // 指向该无重复子串左边的起始位置
+	count := make(map[byte]int, 0)
+	for r := 0; r < len(s); r++ {
+		// if left, ok := count[s[r]]; ok {
+		// 	for ; l <= left; l++ { // l指向重复字符下一个字符
+		// 		delete(count, s[l]) // 删除到重复字符之间的字符
+		// 	}
+		// }
+		if left, ok := count[s[r]]; ok && left >= l {
+			l = left + 1
+		}
+		count[s[r]] = r
+		ret = max(ret, r-l+1)
+	}
+	return ret
+}
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
