df44f184dcce097641ca5624d21bb9e051d481a9
qiuye <1119345739@qq.com>
2022-02-15 22:49:18 +0800

    0141.环形链表

---
+++ leetcode/go/141.环形链表.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=golang
+ *
+ * [141] 环形链表
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
