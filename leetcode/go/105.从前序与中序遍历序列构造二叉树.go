1340149fd2beebbb34cf4a56dd2c81bb06f477cc
qiuye <1119345739@qq.com>
2022-03-12 16:04:59 +0800

    0105.从前序与中序遍历序列构造二叉树

---
+++ leetcode/go/105.从前序与中序遍历序列构造二叉树.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=golang
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	idx := -1
+	rootVal := preorder[0]
+	for i, num := range inorder {
+		if num == rootVal {
+			idx = i
+			break
+		}
+	}
+	return &TreeNode{
+		Val:   preorder[0],
+		Left:  buildTree(preorder[1:idx+1], inorder[:idx]),
+		Right: buildTree(preorder[idx+1:], inorder[idx+1:]),
+	}
+}
+
+// @lc code=end
