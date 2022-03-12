f2c0f94d52a3b46b01ed5dc9fd8f4ae164355513
qiuye <1119345739@qq.com>
2022-03-12 19:27:49 +0800

    0617.合并二叉树

---
+++ leetcode/go/617.合并二叉树.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=golang
+ *
+ * [617] 合并二叉树
+ */
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	} else if root2 == nil {
+		return root1
+	}
+	root := &TreeNode{Val: root1.Val + root2.Val}
+	root.Left = mergeTrees(root1.Left, root2.Left)
+	root.Right = mergeTrees(root1.Right, root2.Right)
+	return root
+}
+
+// @lc code=end
