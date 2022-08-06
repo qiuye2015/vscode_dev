0ab5a6fc57fcb255e86dfab6a1b1abea95c7932f
qiuye2015 <1119345739@qq.com>
2022-08-06 13:13:12 +0800

    0153.寻找旋转排序数组中的最小值

---
+++ leetcode/go/153.寻找旋转排序数组中的最小值.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=golang
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+package main
+
+// @lc code=start
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else if nums[mid] <= nums[right] {
+			right = mid
+		}
+	}
+	return nums[left]
+}
+
+// @lc code=end
