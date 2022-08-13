6d1430e050894a044aaf3d6a02206558b69d0fa3
qiuye2015 <1119345739@qq.com>
2022-08-13 22:56:57 +0800

    0235.二叉搜索树的最近公共祖先

---
+++ leetcode/go/235.二叉搜索树的最近公共祖先.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=golang
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+package main
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
+
+// @lc code=end
